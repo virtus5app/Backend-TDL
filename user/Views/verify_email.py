d0609af1cb408a67b23dc48d0480ffc224f48123
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2023-11-05 13:45:18 +0100

    verifies account

--- user/Views/verify_email.py
+++ user/Views/verify_email.py
@@ -3,15 +3,20 @@ from rest_framework.response import Response
 from ..models import MyUser as User
 from django.conf import settings
 from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
 def verify_email( request):
-    token = request.GET.get('token')
+
+    token = request.data['token']
+
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
-        # print(payload)
-        # user = User.objects.get(id=payload['user_id'])
-        # if not user.is_verified:
-        #     user.is_verified = True
-        #     user.save()
+        user = User.objects.get(id=payload['user_id'])
+        if not user.is_verified:
+            user.is_verified = True
+            user.save()
         return Response({'message': 'Successfully activated'}, status=status.HTTP_200_OK)
     except jwt.ExpiredSignatureError as identifier:
         return Response({'message': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)

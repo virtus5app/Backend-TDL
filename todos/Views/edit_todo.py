8f45fd5fa3336e962292e7d15a6fdb1b5f3d2599
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2023-11-08 18:09:03 +0100

    edit todo completed

---
+++ todos/Views/edit_todo.py
@@ -0,0 +1,29 @@
+from ..models import Todo
+from rest_framework.response import Response
+from rest_framework import status
+from Utils.get_user_id import get_user_id
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from ..serializer.Todoserializer import TodoSerializer
+
+@api_view(['PUT'])
+def EditTodo(request, todo_uuid):
+    try:
+        todo = get_object_or_404(Todo, pk=todo_uuid)
+        print(request.data)
+
+        if todo is None:
+            return Response({"message": "Todo not found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Update the Todo instance with the new data
+        todo.title = request.data['title']
+        todo.date = request.data['date']
+        todo.completed = request.data['completed']
+        todo.save()
+
+        # Serialize the updated Todo instance
+        serializer = TodoSerializer(todo)
+
+        return Response({"message": "Task Edited", "todo": serializer.data}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .Views.get_todos import GetTodos
 from .Views.add_todos import AddTodos
+from .Views.edit_todo import EditTodo
 
 urlpatterns = [
     path('get-todos/', GetTodos, name='get-todos'),
     path('add-todos/', AddTodos, name='add-todos'),
+    path('edit-todos/<uuid:todo_uuid>/', EditTodo, name='edit-todos')
 
     ]
